30ddcf013f4850c6f2ad1a32ca0008029bfaa751
Sean Griffin <sean@thoughtbot.com>
2014-07-18 19:02:37 -0600

    Pass normals

--- app/assets/shaders/main_fragment.glsl
+++ app/assets/shaders/main_fragment.glsl
@@ -1,9 +1,11 @@
 precision mediump float;
 
 varying vec2 fragmentUv;
+varying vec4 diffuseColor;
 
 uniform sampler2D textureSampler;
 
 void main() {
-  gl_FragColor = texture2D(textureSampler, vec2(fragmentUv.s, fragmentUv.t));
+  vec4 textureColor = texture2D(textureSampler, vec2(fragmentUv.s, fragmentUv.t));
+  gl_FragColor = textureColor * diffuseColor;
 }
--- app/assets/shaders/main_vertex.glsl
+++ app/assets/shaders/main_vertex.glsl
@@ -1,12 +1,22 @@
 attribute vec3 vertexCoord;
 attribute vec2 vertexUv;
+attribute vec3 vertexNormal;
 
 varying vec2 fragmentUv;
+varying vec4 diffuseColor;
 
 uniform mat4 mvp;
+
+const vec4 lightPosition = vec4(-1.0, 1.0, -1.0, 0.0);
+const vec4 lightDiffuseColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+const vec4 materialDiffuseColor = vec4(1.0, 0.8, 0.8, 1.0);
 
 void main() {
   gl_Position = mvp * vec4(vertexCoord, 1);
 
+  vec3 adjustedLightPosition = normalize(vec3(lightPosition)) * vec3(-1.0, -1.0, -1.0);
+
   fragmentUv = vertexUv;
+  diffuseColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
