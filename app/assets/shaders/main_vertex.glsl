90d35d40f3d3bc17b6a8e7fcac1da50be142c965
Sean Griffin <sean@thoughtbot.com>
2014-07-20 21:50:35 -0600

    Apply skins (slowly...)

--- app/assets/shaders/main_vertex.glsl
+++ app/assets/shaders/main_vertex.glsl
@@ -14,9 +14,30 @@ uniform mat3 normalMatrix;
 uniform mat4 bones[154];
 
 void main() {
-  vec4 wibble = vertexSkinWeights * vertexSkinIndices;
-  fragmentPosition = m * vec4(vertexCoord, 1);
+  vec4 skinnedVertex;
+  vec4 skinnedNormal;
+
+  vec4 vertex = vec4(vertexCoord, 1.0);
+  vec4 normal = vec4(vertexNormal, 0.0);
+
+  int index = int(vertexSkinIndices.x);
+  skinnedVertex = bones[index] * vertex * vertexSkinWeights.x;
+  skinnedNormal = bones[index] * normal * vertexSkinWeights.x;
+
+  index = int(vertexSkinIndices.y);
+  skinnedVertex += bones[index] * vertex * vertexSkinWeights.y;
+  skinnedNormal += bones[index] * normal * vertexSkinWeights.y;
+
+  index = int(vertexSkinIndices.z);
+  skinnedVertex += bones[index] * vertex * vertexSkinWeights.z;
+  skinnedNormal += bones[index] * normal * vertexSkinWeights.z;
+
+  index = int(vertexSkinIndices.w);
+  skinnedVertex += bones[index] * vertex * vertexSkinWeights.w;
+  skinnedNormal += bones[index] * normal * vertexSkinWeights.w;
+
+  fragmentPosition = m * skinnedVertex;
   gl_Position = p * v * fragmentPosition;
   fragmentUv = vertexUv;
-  fragmentNormal = normalMatrix * vertexNormal;
+  fragmentNormal = normalMatrix * vec3(skinnedNormal);
 }
